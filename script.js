08f9f10dd795db765affbbd6b04a957cc85f221c
bdiddy <44068902+bmdito@users.noreply.github.com>
2020-04-30 15:27:08 -0700

    get request and jquery test

--- script.js
+++ script.js
@@ -10,17 +10,43 @@
 
 $("#demo-butt").on("click", function () {
   console.log("you clicked demo button");
+  var queryURL = "https://api.giphy.com/v1/gifs/trending?api_key=dc6zaTOxFJmzC";
+
+  $.ajax({
+    url: queryURL,
+    method: "GET",
+    // headers: {
+    //   "Access-Control-Allow-Credentials": true,
+    //   "Access-Control-Allow-Origin": "*",
+    //   "Access-Control-Allow-Methods": "GET",
+    //   "Access-Control-Allow-Headers": "application/json",
+    // },
+  }).then(function (response) {
+    // response.setHeader("Set-Cookie", "HttpOnly;Secure;SameSite=Strict");
+    console.log(response);
+    var imageurl = response.data[0].image_original_url;
+    console.log(imageurl);
+
+    var makePic = $("<img>");
+    makePic.attr("src", imageurl);
+    makePic.attr("alt", "sexy man");
+
+    $("#demo-section").append(makePic);
+  });
 });
 
-// function runVanilla(callback) {
-//   console.log("clicked other demo");
-//   var theUrL = "https://api.giphy.com/v1/gifs/trending?api_key=dc6zaTOxFJmzC";
+function runVanilla() {
+  var xhr = $.get(
+    "http://api.giphy.com/v1/gifs/search?q=ryan+gosling&api_key=dc6zaTOxFJmzC&limit=5"
+  );
 
-//   var xmlHttp = new XMLHttpRequest();
-//   xmlHttp.onreadystatechange = function () {
-//     if (xmlHttp.readyState == 4 && xmlHttp.status == 200)
-//       callback(xmlHttp.responseText);
-//   };
-//   xmlHttp.open("GET", theUrl, true); // true for asynchronous
-//   xmlHttp.send(null);
-// }
+  xhr.done(function (data) {
+    console.log("success got data", data);
+  });
+}
+
+$("#test3").on("click", function () {
+  let newDiv = $("<p></p>");
+  newDiv.text("Hello world");
+  $("#demo-2").append(newDiv);
+});
